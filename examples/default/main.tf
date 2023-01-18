bffa0904c95fc6ccbdebf71f881dcdcb3e38184e
Robert de Bock <robert@meinit.nl>
2023-01-18 13:30:01 +0100

    Concurrency is calculated.

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,6 +1,5 @@
 # Call the module
 module "gitlab_runner" {
   source                           = "../../"
-  gitlab_runner_concurrency        = 4
   gitlab_runner_registration_token = "GET_YOUR_OWN_TOKEN_PLEASE"
 }
